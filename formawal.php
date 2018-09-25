59013f3936d29215a81d4b7d6d8ee8cb765a3c6d
Dian Widya Putri <43510248+dianwp@users.noreply.github.com>
2018-09-25 16:53:53 +0700

    Add files via upload

--- formawal.php
+++ formawal.php
@@ -1,5 +1,40 @@
+<form method="POST" action="proses.php">
 <?php
-
-
-
-?>
+session_start();
+error_reporting(0);
+        $user = array(
+                        "user" => "dianwp",
+                        "pass"=>"2008"            
+                );
+if (isset($_POST['submit'])) {
+    if ($_POST['username'] == $user['user'] && $_POST['password'] == $user['pass']){
+        $_SESSION["username"] = $_POST['username']; 
+        echo "Anda Berhasil Login $_POST[username]";
+    } else {
+        display_login_form();
+        echo '<p>Username Atau Password Tidak Benar</p>';
+    }
+}    
+else { 
+    display_login_form();
+}
+function display_login_form(){ ?>
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method='post'>
+        <center>
+    <table>
+<tr>
+    <td><label for="username">Username</label></td>
+    <td><input type="text" name="username" id="username" style="width: 170px;height: 30px"></td>
+</tr>
+<tr>
+    <td><label for="password">Password</label></td>
+    <td><input type="password" name="password" id="password" style="width: 170px;height: 30px"></td>
+</tr>
+<tr>
+    <td></td>
+    <td><input type="submit" name="submit" value="submit" style="color: black; width: 100px;height: 40px;border-radius: 10px"</td>
+</tr>
+</table>
+</center>
+    </form>    
+<?php } ?>
--- proses.php
+++ proses.php
@@ -1,4 +1,26 @@
+<html>
+<head>
+<title></title>
+</head>
+<body>
+<form method="POST" enctype="multipart/form-data">
+<input type="file" name="Foto"></br>
+Hobbi : </br>
+<input type="checkbox" name="hobi[]" value="Makan">Makan </br>
+<input type="checkbox" name="hobi[]" value="Minum">Minum </br>
+<input type="checkbox" name="hobi[]" value="Tidur">Tidur </br>
+<input type="checkbox" name="hobi[]" value="Browsing">Dengerin musik</br>
+<input type="submit" name="submit" value="Input">
+</form>
+</body>
+</html>
+
 <?php
 
+if (isset($_POST['submit'])) {
+	$foto=$_POST['Foto'];
+	$hobbi= $_POST['hobbi'];
+	echo "<img src='Gambar'>";
+}
 
 ?>
